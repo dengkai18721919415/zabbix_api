94165ff1fa63b7476f9d9d082b76374398a42ba1
dengkai18721919415 <32827121+dengkai18721919415@users.noreply.github.com>
2017-10-30 05:07:19 +0800

    Update test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python
+#coding:utf-8
+import json
+import urllib2
+import sys
 
+
+calss zabbixtools:
+    def __init__(self):
+        self.url = "http://company/zabbix/api_jsonrpc.php"
+        self.header = {"Content-Type":"application/json"}
+        self.authID = self.user_login()
+    
+    def user_login(self):
+        data = json.dumps(
+            {
+                "jsonrpc": "2.0",
+                "method": "user.login",
+                "params": {
+                    "user": "Admin",
+                    "password" :"xxxx"
+                },
+                "id": 0
+            })
