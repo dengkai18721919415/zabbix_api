d9f61b3a0954e2b99128fb39d74893951704984a
dengkai18721919415 <32827121+dengkai18721919415@users.noreply.github.com>
2017-10-30 07:57:19 +0800

    Update test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -22,3 +22,26 @@ calss zabbixtools:
                 },
                 "id": 0
             })
+    request = urllib2.Request(self.url,data)
+    for key in self.header:
+        request.add_header(key,self.header[key])
+    try:
+        result = urllib2.urlopen(request)
+    except URLError as e:
+        response = json.loads(result.read())
+        result.close()
+        authID = response['result']
+        return authID
+    
+    def get_data(self,data,hostip=""):
+        request = urllib2.Request(self.url,data)
+        for key in self.header:
+            request.add_header(key,self.header[key])
+        try:
+            result = urllib2.urlopen(request)
+        except URLError as e:
+            if hasattr(e, 'reason'):
+                print 'Error:We have failed to reach the server!'
+                print 'Reason: ',e.reason
+        elif hasattr(e, 'code'):
+            
